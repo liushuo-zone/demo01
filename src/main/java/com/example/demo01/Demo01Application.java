cb380c6f1f849d5e1175541cff9fc85d83dacdec
liushuo <liushuo002@icloud.com>
2024-12-05 23:18:05 +0800

    init

--- src/main/java/com/example/demo01/Demo01Application.java
+++ src/main/java/com/example/demo01/Demo01Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo01Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Demo01Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Demo01Application.class, args);
+    }
 
 }
